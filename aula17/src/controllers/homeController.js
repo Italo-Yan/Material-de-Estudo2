067184f0c66acc93d0521058d3883032732910b3
Italo-Yan <italoyan2004@gmail.com>
2023-07-28 10:30:34 -0300

    comentarios commit

--- aula17/src/controllers/homeController.js
+++ aula17/src/controllers/homeController.js
@@ -1,3 +1,4 @@
+// Controlador da página inicial, essa função é responsável por renderizar a visualização 'index' a visualização recebe alguns dados como parâmetros para serem utilizados na renderização.
 exports.paginaInicial = (req, res) => {
     res.render('index', {
         titulo: 'Este será o título da página',
@@ -7,6 +8,7 @@ exports.paginaInicial = (req, res) => {
     return;
 };
 
+// Controlador para tratamento de um POST, essa função é responsável por tratar uma requisição HTTP do tipo POST. Ela envia de volta ao cliente a informação contida no corpo (body) da requisição, essa resposta é geralmente utilizada para confirmar que a informação foi recebida e processada corretamente pelo servidor.
 exports.trataPost = (req, res) => {
     res.send(req.body);
     return;
--- aula17/src/middlewares/middleware.js
+++ aula17/src/middlewares/middleware.js
@@ -1,18 +1,26 @@
+// Esse middleware é executado para todas as rotas da aplicação.
+// Ele adiciona uma variável local chamada "umaVariavelLocal" ao objeto "res" com o valor 'Este é o valor da variável local'.
 exports.middlewareGlobal = (req, res, next) => {
     res.locals.umaVariavelLocal = 'Este é o valor da variável local';
     next();
 };
 
+// Esse middleware é executado para todas as rotas da aplicação.
 exports.outroMiddleware = (req, res, next) => {
     next();
 };
 
+// Esse middleware é usado para verificar se ocorreu algum erro relacionado ao token CSRF.
+// Se o erro existir e o código do erro for igual a 'EBADCSRFTOKEN', o middleware renderiza o arquivo "404.ejs" localizado na pasta de visualizações (views).
+// Esse middleware é comumente usado para proteção contra ataques CSRF em formulários.
 exports.checkCsrfError = (err, req, res, next) => {
-    if(err && 'EBADCSRFTOKEN' === err.code) {
-        return res.render('../views//404.ejs');
+    if (err && 'EBADCSRFTOKEN' === err.code) {
+        return res.render('../views/404.ejs');
     }
 };
 
+// Esse middleware é responsável por adicionar um token CSRF (geralmente um valor aleatório) à variável local "csrfToken" do objeto "res".
+// Esse token é usado para proteger as rotas contra ataques CSRF em formulários e outras solicitações.
 exports.csrfMiddelware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
