8b013ec404b314b8a9c3da51c3f990749bc940cd
Italo-Yan <italoyan2004@gmail.com>
2023-08-04 14:30:42 -0300

    curso commit

--- aula18/src/middlewares/middleware.js
+++ aula18/src/middlewares/middleware.js
@@ -1,5 +1,5 @@
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da variável local';
+    res.locals.errors = req.flash('errors');
     next();
 };
 
@@ -9,7 +9,7 @@ exports.checkCsrfError = (err, req, res, next) => {
         return res.render('404');
     }
 
-    next()
+    next();
 };
 
 
