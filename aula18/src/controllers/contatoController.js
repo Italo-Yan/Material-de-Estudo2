f7a1da9ab950150a9d9b56f6a382b326f0c42972
italo-yan <italoyan2004@gmail.com>
2023-08-17 12:07:57 -0300

    ajustes e correções commit

--- aula18/src/controllers/contatoController.js
+++ aula18/src/controllers/contatoController.js
@@ -1,27 +1,27 @@
 const Contato = require('../models/ContatoModel');
 
 exports.index = (req, res) => {
-    res.render('contato', {
-        contato: {}
-    });
+  res.render('contato', {
+    contato: {}
+  });
 };
 
-exports.register = async (req, res) => {
-    try {
-        const contato = new Contato(req.body);
-        await contato.register();
+exports.register = async(req, res) => {
+  try {
+    const contato = new Contato(req.body);
+    await contato.register();
 
-        if(contato.errors.length > 0) {
-            req.flash('errors', contato.errors);
-            req.session.save(() => res.redirect('back')); 
-            return;
-        }
-
-        req.flash('success', 'Contato registrado com sucesso.'); 
-        req.session.save(() => res.redirect('back')); 
-        return;
-    } catch(e) {
-        console.log(e);
-        res.render('404');
+    if(contato.errors.length > 0) {
+      req.flash('errors', contato.errors);
+      req.session.save(() => res.redirect('back'));
+      return;
     }
+
+    req.flash('success', 'Contato registrado com sucesso.');
+    req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+    return;
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
 };
